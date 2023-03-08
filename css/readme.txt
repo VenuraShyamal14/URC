330dbdff0d69ebd04ad9d5c8a963076034f72933
Venura Shyamal <shamalbandara14@gmail.com>
2023-03-08 11:42:02 +0530

    card links added

---
+++ css/readme.txt
@@ -0,0 +1,4 @@
+style sheet eka bootsrap ekat passe danna
+stop nav bar overflow
+make large text for navbar
+add some effects to navbar
--- sub topics/staff intranet/events.php
+++ sub topics/staff intranet/events.php
@@ -1,17 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>URC</title>
-    <link href="../../css/styles.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-   
-
-</head>
-<body>
-
+<!--header-->
+<?php
+      include('../../header.html');
+  ?>
 
      <!--navbar-->
      <?php
@@ -20,8 +10,7 @@
 
     <h2>this is events page</h2>
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-    <script src="../../js/index.js" charset="utf-8"></script>
-</body>
-</html>
+<!--footer-->
+<?php
+      include('../../footer.html');
+  ?>
