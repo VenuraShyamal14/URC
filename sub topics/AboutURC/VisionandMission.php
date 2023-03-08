d79a7a6a51cf589e93b0aa62607f3957b7cd69fd
Venura Shyamal <shamalbandara14@gmail.com>
2023-03-08 19:04:48 +0530

    vision and mission content

--- sub topics/AboutURC/VisionandMission.php
+++ sub topics/AboutURC/VisionandMission.php
@@ -8,7 +8,19 @@
     include('../../navbar.html');
      ?>
 
-    <h2>this is Vision_and_Mission page</h2>
+        <section class="top">
+        <h1>OUR MISSION</h1>
+        <p>
+            If you're looking for random paragraphs, you've come to the right place. When a random word or a random sentence isn't quite enough, the next logical step is to find a random paragraph. We created the Random Paragraph Generator with you in mind. The process is quite simple
+        </p>
+    </section>
+
+    <section class="bottom ">
+        <h1>OUR VISSION</h1>
+        <p>
+            If you're looking for random paragraphs, you've come to the right place. When a random word or a random sentence isn't quite enough, the next logical step is to find a random paragraph. We created the Random Paragraph Generator with you in mind. The process is quite simple
+        </p>
+    </section>
 
 <!--footer-->
 <?php
