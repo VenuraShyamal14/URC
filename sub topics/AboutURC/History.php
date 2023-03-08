b1dd93615ef0dc853ae283c2dd982d56c9c00494
Venura Shyamal <shamalbandara14@gmail.com>
2023-03-08 19:08:41 +0530

    added history page

--- sub topics/AboutURC/History.php
+++ sub topics/AboutURC/History.php
@@ -10,7 +10,20 @@
    include('../../navbar.html');
     ?>
   
-    <h2>this is History page</h2>
+    <section class="top bottom">
+        <h1>
+            History of University Research Counsil
+        </h1>
+        <p>
+        Recognizing the importance of promoting research among staff of the university, the International Research Centre (InRC) was established at University of Peradeniya in 2012.
+
+The Main activities of the InRC included enhancing global relations, promoting multidisciplinary collaborative research programmes, and facilitating students and staff of the university by providing and coordinating resources, platforms and scholarly exchanges.
+
+This centre has been functioning as the liaising entity for all international exchanges and research activities of the university and has initiated and renewed links and agreements of the university with various institutions throughout the world. Since inception the InRC was headed by a full time director until it was transformed into International Affairs Office (IAO) in 2018.
+
+The main focus of the IAO was to co-ordinate international affairs of the University, therefore, the need of an entity to further research was felt. In order to cater to the need of promoting research activities the university proposed that URC be established. Accordingly, the URC was established in 2020. The URC strives for facilitating research within university of Peradeniya by creating a conducive environment for the university community.
+        </p>
+    </section>
 
 <!--footer-->
 <?php
