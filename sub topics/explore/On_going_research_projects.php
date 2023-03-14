ef438620119a5b7b268795444317af4f00eb5799
Venura Shyamal <shamalbandara14@gmail.com>
2023-03-14 18:21:17 +0530

    add youtube videos

--- sub topics/explore/On_going_research_projects.php
+++ sub topics/explore/On_going_research_projects.php
@@ -2,13 +2,38 @@
 <?php
       include('../../header.html');
   ?>
+  
 
    <!--navbar-->
    <?php
    include('../../navbar.html');
     ?>
 
-    <h2>this is on On_going_research_projects page</h2>
+
+    <div class="d-flex flex-row mb-3">
+        <div class="p-2"><iframe src="https://www.youtube.com/embed/TZtjTploF8c" allowfullscreen></iframe></div>
+        <div class="p-2">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+    </div>
+
+    <div class="d-flex flex-row-reverse mb-3">
+        <div class="p-2"><iframe src="https://www.youtube.com/embed/TZtjTploF8c" allowfullscreen></iframe></div>
+        <div class="p-2">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+    </div>
+
+    <div class="d-flex flex-row mb-3">
+        <div class="p-2"><iframe src="https://www.youtube.com/embed/TZtjTploF8c" allowfullscreen></iframe></div>
+        <div class="p-2">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+    </div>
+
 
 <!--footer-->
 <?php
