0171d55ef87fb0606913ea2a12a5912061ef4849
Venura Shyamal <shamalbandara14@gmail.com>
2023-03-19 19:05:58 +0530

    remove php loading and used iframe

--- js/index.js
+++ js/index.js
@@ -1,10 +1,27 @@
+const popup = document.getElementById("popupid");
+const backBtn = document.getElementById("backBtn");
+const content = document.getElementById("loadHtml");
+const buttons = document.querySelectorAll('.clickname');
 
-let popup=document.getElementById("popupid");
 
-function openPopup(){
+// Loop through and add a click event listener to each one of tr
+buttons.forEach(function(button) {
+  button.addEventListener('click', function(event) {
+    // Get the class of the clicked button
+    const clickedText = event.target.classList[1];
+    
+    // Log the clicked text to the console
+    console.log(clickedText);
+
+    //change the html
+    content.src = '/new.html';
+
+    //open popup
     popup.classList.add("open-popup");
-}
+  });
+});
 
-function closePopup(){
-    popup.classList.remove("open-popup");
-}
+backBtn.addEventListener('click', function() {
+  popup.classList.remove("open-popup");
+});
+
--- sub topics/explore/Academic_publications.php
+++ sub topics/explore/Academic_publications.php
@@ -13,14 +13,12 @@
     <div class="popup" id="popupid">
         <img src="/img/publications.jpg" alt="publications Icon" width="100" height="100" >
         <h2>PUBLICATIONS</h2>
-        <div class="publication-content">
-            <?php
-                include('../../'+button_name+'.html');
-            ?>
         
-            
-            </div>
-        <button type="button" class="btn btn-primary" onclick="closePopup()">back</button>
+        <div id="pubConId" class="publication-content">
+            <iframe id="loadHtml" width="100%" height="100%" src="/publications.html"></iframe>
+        </div>
+        
+        <button id="backBtn" type="button" class="btn btn-primary">back</button>
     </div>
 
 
@@ -40,24 +38,24 @@
                 </tr>
               </thead>
               <tbody>
-                <tr onclick="openPopup()">
+                <tr >
                   <td scope="row">01</td>
-                  <td>Dr. SAHA Suraweera</td>
+                  <td class="clickname SAHASuraweera">Dr. SAHA Suraweera</td>
                   <td>Engineering</td>
                 </tr>
-                <tr onclick="openPopup()">
+                <tr>
                   <td scope="row">02</td>
-                  <td>Prof. BC Jayawardana</td>
+                  <td class="clickname BCJayawardana">Prof. BC Jayawardana</td>
                   <td>Agriculture</td>
                 </tr>
-                <tr onclick="openPopup()">
+                <tr>
                     <td scope="row">03</td>
-                    <td>Prof. KMS Wimalasiri</td>
+                    <td class="clickname KMSWimalasiri">Prof. KMS Wimalasiri</td>
                     <td>Agriculture</td>
                 </tr>
-                <tr onclick="openPopup()">
+                <tr>
                     <td scope="row">04</td>
-                    <td>Prof. DNB Gunawardane</td>
+                    <td class="clickname DNBGunawardane">Prof. DNB Gunawardane</td>
                     <td>Arts</td>
                 </tr>
               </tbody>
@@ -70,15 +68,3 @@
       include('../../footer.html');
   ?>
 
-<script>
-    let popup=document.getElementById("popupid");
-
-    function openPopup(){
-        popup.classList.add("open-popup");
-    }
-
-    function closePopup(){
-        popup.classList.remove("open-popup");
-    }
-
-</script>
