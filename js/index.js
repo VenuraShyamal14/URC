8b9d8f54b25c330e0a5e3e6ba13def90f0a2f900
Venura Shyamal <shamalbandara14@gmail.com>
2023-04-07 19:34:47 +0530

    search publications by faculty is done

--- js/index.js
+++ js/index.js
@@ -1,62 +1,70 @@
 
-function search(pub) {
-  var data = {
-      publication: pub,
-      
-   
-  };
-
-  var xhr = new XMLHttpRequest();
-
-  // set the PHP page you want to send data to
-  xhr.open("POST", "../../search.php", true);
-  xhr.setRequestHeader("Content-Type", "application/json");
-
-  // what to do when you receive a response
-  xhr.onreadystatechange = function () {
-      if (xhr.readyState == XMLHttpRequest.DONE) {
-          
-          var returned_data=xhr.responseText;
-          //alert(returned_data);
-          //alert(xhr.responseText);
-          //paragraph.innerHTML=xhr.responseText;
-      }
-  };
-
-  // send the data
-  xhr.send(JSON.stringify(data));
-}
-
 
 let myObj;
 function searchData(id) {
-  var data = {
-      Id: id,
-  };
+    var data = {
+        Id: id,
+    };
 
-  var xhr1 = new XMLHttpRequest();
+    var xhr1 = new XMLHttpRequest();
 
-  // set the PHP page you want to send data to
-  xhr1.open("POST", "search_data.php", true);
-  xhr1.setRequestHeader("Content-Type", "application/json");
+    // set the PHP page you want to send data to
+    xhr1.open("POST", "search_by_faculty.php", true);
+    xhr1.setRequestHeader("Content-Type", "application/json");
 
-  // what to do when you receive a response
-  xhr1.onreadystatechange = function () {
-      if (xhr1.readyState == XMLHttpRequest.DONE) {
-          
-          //var returned_data=xhr.responseText;
-          //alert(returned_data);
-          myObj = JSON.parse(this.responseText);
-          displayData(myObj);
-          //alert(myObj[0].faculty);
-          //paragraph.innerHTML=xhr.responseText;
-      }
-      //xhr1.open("GET", "search_data.php");
-  };
+    // what to do when you receive a response
+    xhr1.onreadystatechange = function () {
+        if (xhr1.readyState == XMLHttpRequest.DONE) {
+            
+            //var returned_data=xhr.responseText;
+            //alert(returned_data);
+            myObj = JSON.parse(this.responseText);
+            displayData(myObj);
+            //alert(myObj[0].faculty);
+            //paragraph.innerHTML=xhr.responseText;
+        }
+        //xhr1.open("GET", "search_data.php");
+    };
 
-  // send the data
-  xhr1.send(JSON.stringify(data));
+    // send the data
+    xhr1.send(JSON.stringify(data));
 }
 
 
 
+let myPub;
+function searchPublication(id) {
+    var data = {
+        Id: id,
+    };
+
+    var xhr2 = new XMLHttpRequest();
+
+    // set the PHP page you want to send data to
+    xhr2.open("POST", "search_for_publication.php", true);
+    xhr2.setRequestHeader("Content-Type", "application/json");
+
+    // what to do when you receive a response
+    xhr2.onreadystatechange = function () {
+        if (xhr2.readyState == XMLHttpRequest.DONE) {
+            
+            //var returned_data=xhr.responseText;
+            //alert(returned_data);
+            myPub = JSON.parse(this.responseText);
+            //alert(myPub);
+            //displayData(myPub);
+            //alert(myObj[0].faculty);
+            //search_results.innerHTML=myPub;
+            let text1="<hr>";
+            for (let k = 0; k < myPub.length; k++) {
+                text1 =text1.concat(myPub[k].publication);
+                text1 =text1.concat("<hr>");
+            }
+            search_results.innerHTML =text1;
+        }
+        //xhr1.open("GET", "search_data.php");
+    };
+
+    // send the data
+    xhr2.send(JSON.stringify(data));
+}
---
+++ sub topics/explore/search_for_publication.php
@@ -0,0 +1,26 @@
+    <?php 
+    include_once("../../db_connect.php");
+    ?>
+
+<?php
+
+$data = json_decode(file_get_contents("php://input"), true);
+$id=$data["Id"];
+
+mysqli_set_charset($conn, "utf8mb4");
+$query1 = "SELECT * FROM publications WHERE faculty = '$id'";
+//$query1 = "SELECT * FROM cards WHERE id = 1";
+$result1 = mysqli_query($conn, $query1);
+//$researcher = mysqli_fetch_assoc($result1);
+
+$array1 = array();
+while($researcher = mysqli_fetch_assoc($result1) ) {
+
+    $array1[] = $researcher;
+
+}
+
+
+echo json_encode($array1, JSON_UNESCAPED_UNICODE);
+
+mysqli_close($conn);
